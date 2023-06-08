3b132d8dfa08d190609e306fb010a3d1ab965b93
gMarcondes00 <g.os.marcondes@gmail.com>
2023-06-08 17:55:37 -0300

    função iniciar e atirar adicionada

--- main.c
+++ main.c
@@ -1,6 +1,178 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+//Funções a serem utilizadas
+void alocar(char matriz[10][10]); //chamada para receber as coordenadas e o navio a ser alocado
+int alocarGrandes (char matriz[10][10], int x, int y, int direcao, int tamanho); //alocar um navio em uma coordenada específica
+void Iniciar(char matriz1[10][10], char matriz2[10][10]); //Função para iniciar o jogo
+int atirar(char matriz[10][10], int x, int y); // Função para pegar coordenadas e matriz a ser comparada
+
+int main() {
+    printf("\n--------- Batalha Naval ---------\n\n");
+    int i, j;
+    char P1[10][10]; char P2[10][10];
+
+    //inicializar a matriz com '-'
+    for(i = 0; i < 10; i++){
+        for(j = 0; j < 10; j++){
+            P1[i][j] = '-';
+            P2[i][j] = '-';
+        }
+    }
+
+    int escolha;
+    bool ficar = true;
+    while(ficar){
+    
+    //Mostrar as matrizes dos dois jogadores:
+    printf("\n");
+    for(i = 0; i < 10; i++){
+        for(j = 0; j < 10; j++){
+            printf("%c ",P1[i][j]);
+        }
+        printf("        ");
+        for(j = 0; j < 10; j++){
+            printf("%c ", P2[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n   Menu do Jogo:\n\n1 - Alocar Navios\n2 - Iniciar o Jogo\n3 - Sair do Jogo\n");
+    printf("Selecione uma opcao: ");
+    scanf("%d", &escolha);
+
+    int player;
+    //Alternar entre escolhas do Menu
+    switch(escolha){
+        case 1:
+            printf("Alocar navio de qual jogador? 1 ou 2: ");
+            scanf("%d", &player);
+            if(player == 2) {
+                alocar(P2);
+            }
+            if(player == 1) {
+                alocar(P1);
+            }
+            break;
+        case 2:
+            Iniciar(P1, P2);
+            break;
+        case 3:
+            ficar = false;
+            break;
+        default:
+            printf("Opcao invalida");
+            break;
+    }}
+
+    return 0;
+}
+
+// função pegar coordenadas que o usuario quer alocar
+// alocar submarinos na matriz
+void alocar(char matriz[10][10]){
+    int n[4];
+    n[0] = 1, n[1] = 2, n[2] = 3, n[3] = 4;
+    //inicializar a matriz com '-' caso o usuario queira realocar novamente
+    for(int i = 0; i < 10; i++){
+        for(int j = 0; j < 10; j++){
+            matriz[i][j] = '-';
+        }
+    }
+
+    int loop = n[0] + n[1] + n[2] + n[3];
+    while(loop > 0) {
+        printf("\n\n");
+        for(int i = 0; i < 10; i++){
+            for(int j = 0; j < 10; j++){
+            printf("%c ",matriz[i][j]);
+            }
+            printf("\n");
+        }
+        int x, y, navio, direcao, sucesso;
+        printf("\nNavios :\n\n0 - Porta-Avioes(%d) = 4 blocos\n1 - Navios-Cargueiros(%d) = 3 blocos\n2 - Contratorpedeiros(%d) = 2 blocos\n3 - Submarinos(%d) = 1 bloco\n", n[0], n[1], n[2], n[3]);
+        
+        printf("selecione um navio: \n");
+        scanf("%d", &navio);
+        switch (navio){
+            //porta-avioes
+            case 0:
+                printf("qual as coordenadas(linha e coluna, respectivamente) do ponto que gostaria de inserir?\n");
+                scanf("%d %d", &x, &y);
+
+                printf("Em qual rotacao? Direita(0), Esquerda(1), Cima(2), Baixo(3)\n");
+                scanf("%d", &direcao);
+
+                if(n[0] > 0){
+                    sucesso = alocarGrandes(matriz, x, y, direcao, 4);
+                    if(sucesso == 1) {
+                    --n[0];
+                    --loop;
+                    printf("\n\n** Alocado com sucesso **");
+                    } else {
+                    printf("\n\n** Falha ao alocar **");
+                }
+                } 
+
+                break;
+                //navio-cargueiro
+            case 1: 
+                printf("qual as coordenadas(linha e coluna, respectivamente) do ponto que gostaria de inserir?\n");
+                scanf("%d %d", &x, &y);
+
+                printf("Em qual rotacao? Direita(0), Esquerda(1), Cima(2), Baixo(3)\n");
+                scanf("%d", &direcao);
+
+                if(n[1] > 0){
+                    sucesso = alocarGrandes(matriz, x, y, direcao, 3);
+                    if(sucesso == 1) {
+                    --n[1];
+                    --loop;
+                    printf("\n\n** Alocado com sucesso **");
+                    } else {
+                    printf("\n\n** Falha ao alocar **");
+                }
+                } 
+                break;
+                //contratorpedeiro
+            case 2: 
+                printf("qual as coordenadas(linha e coluna, respectivamente) do ponto que gostaria de inserir?\n");
+                scanf("%d %d", &x, &y);
+
+                printf("Em qual rotacao? Direita(0), Esquerda(1), Cima(2), Baixo(3)\n");
+                scanf("%d", &direcao);
+
+                if(n[2] > 0){
+                    sucesso = alocarGrandes(matriz, x, y, direcao, 2);
+                    if(sucesso == 1) {
+                        --n[2];
+                        --loop;
+                        printf("\n\n** Alocado com sucesso **");
+                    } else {
+                    printf("\n\n** Falha ao alocar **");
+                }
+                } 
+                break;
+                //submarino
+            case 3: 
+                printf("qual as coordenadas(linha e coluna, respectivamente) do ponto que gostaria de inserir?\n");
+                scanf("%d %d", &x, &y);
+                if(n[3] > 0 && matriz[x][y] == '-'){
+                    --n[3];
+                    --loop;
+                    matriz[x][y] = 'S';
+
+                    printf("\n\n** Alocado com Sucesso **!");   
+                } else {
+                    printf("\n\n** Falha ao alocar **!");
+                };
+                break;
+            default:
+                break;
+        }
+    }
+}
+
 //Alocar grandes navios na matriz
 int alocarGrandes (char matriz[10][10], int x, int y, int direcao, int tamanho) {
     char letra;
@@ -76,162 +248,46 @@ int alocarGrandes (char matriz[10][10], int x, int y, int direcao, int tamanho) 
     return 0;
 }
 
-// função pegar coordenadas que o usuario quer alocar
-// alocar submarinos na matriz
-void alocar(char matriz[10][10]){
-    int n[4];
-    n[0] = 1, n[1] = 2, n[2] = 3, n[3] = 4;
-    //inicializar a matriz com '-'
-    for(int i = 0; i < 10; i++){
-        for(int j = 0; j < 10; j++){
-            matriz[i][j] = '-';
-        }
-    }
-
-    int loop = n[0] + n[1] + n[2] + n[3];
-    while(loop > 0) {
+void Iniciar(char matriz1[10][10], char matriz2[10][10]){
+    int acertos1 = 0, acertos2 = 0, x, y;
+    while(acertos1 != 20 || acertos2 != 20){
+        printf("\n\n");
         for(int i = 0; i < 10; i++){
             for(int j = 0; j < 10; j++){
-            printf("%c ",matriz[i][j]);
+                printf("%c ",matriz1[i][j]);
+            }
+            printf("        ");
+            for(int j = 0; j < 10; j++){
+                printf("%c ", matriz2[i][j]);
             }
             printf("\n");
         }
-        int x, y, navio, direcao, sucesso;
-        printf("Navios :\n\n0 - Porta-Avioes(%d) = 4 blocos\n1 - Navios-Cargueiros(%d) = 3 blocos\n2 - Contratorpedeiros(%d) = 2 blocos\n3 - Submarinos(%d) = 1 bloco\n", n[0], n[1], n[2], n[3]);
+
+        printf("\nJogador 1 atira em Jogador 2, atirar em qual linha e coluna? respectivamente\n");
+        scanf("%d %d", &x, &y);
+        acertos2 += atirar(matriz2, x, y);
         
-        printf("selecione um navio: \n");
-        scanf("%d", &navio);
-        switch (navio){
-            //porta-avioes
-            case 0:
-                printf("qual as coordenadas(linha e coluna, respectivamente) do ponto que gostaria de inserir?\n");
-                scanf("%d %d", &x, &y);
+        printf("Jogador 2 atira em Jogador 1, atirar em qual linha e coluna? respectivamente\n");
+        scanf("%d %d", &x, &y);
+        acertos1 += atirar(matriz1, x, y);
 
-                printf("Em qual rotacao? Direita(0), Esquerda(1), Cima(2), Baixo(3)\n");
-                scanf("%d", &direcao);
-
-                if(n[0] > 0){
-                    sucesso = alocarGrandes(matriz, x, y, direcao, 4);
-                    if(sucesso == 1) {
-                    --n[0];
-                    --loop;
-                    printf("\n\nAlocado com sucesso\n\n");
-                    } else {
-                    printf("\n\nFalha ao alocar\n\n");
-                }
-                } 
-
-                break;
-                //navio-cargueiro
-            case 1: 
-                printf("qual as coordenadas(linha e coluna, respectivamente) do ponto que gostaria de inserir?\n");
-                scanf("%d %d", &x, &y);
-
-                printf("Em qual rotacao? Direita(0), Esquerda(1), Cima(2), Baixo(3)\n");
-                scanf("%d", &direcao);
-
-                if(n[1] > 0){
-                    sucesso = alocarGrandes(matriz, x, y, direcao, 3);
-                    if(sucesso == 1) {
-                    --n[1];
-                    --loop;
-                    printf("\n\nAlocado com sucesso\n\n");
-                    } else {
-                    printf("\n\nFalha ao alocar\n\n");
-                }
-                } 
-                break;
-                //contratorpedeiro
-            case 2: 
-                printf("qual as coordenadas(linha e coluna, respectivamente) do ponto que gostaria de inserir?\n");
-                scanf("%d %d", &x, &y);
-
-                printf("Em qual rotacao? Direita(0), Esquerda(1), Cima(2), Baixo(3)\n");
-                scanf("%d", &direcao);
-
-                if(n[2] > 0){
-                    sucesso = alocarGrandes(matriz, x, y, direcao, 2);
-                    if(sucesso == 1) {
-                    --n[2];
-                    --loop;
-                    printf("\n\nAlocado com sucesso\n\n");
-                    } else {
-                    printf("\n\nFalha ao alocar\n\n");
-                }
-                } 
-                break;
-                //submarino
-            case 3: 
-                printf("qual as coordenadas(linha e coluna, respectivamente) do ponto que gostaria de inserir?\n");
-                scanf("%d %d", &x, &y);
-                if(n[3] > 0 && matriz[x][y] == '-'){
-                    --n[3];
-                    --loop;
-                    matriz[x][y] = 'S';   
-                };
-                break;
-            default:
-                break;
-        }
+        printf("\n\njogador1: %d acertos \njogador2: %d acertos\n\n", acertos1, acertos2);
     }
+
+    if(acertos2 == 20) {
+        printf("\nO jogador 1 Venceu!\n");
+    } else {
+        printf("\nO jogador 2 Venceu!\n");
+    }  
 }
 
-int main() {
-    printf("\n--------- Batalha Naval ---------\n\n");
-    int i, j;
-    char P1[10][10]; char P2[10][10];
-
-    //inicializar a matriz com '-'
-    for(i = 0; i < 10; i++){
-        for(j = 0; j < 10; j++){
-            P1[i][j] = '-';
-            P2[i][j] = '-';
-        }
+int atirar(char matriz[10][10], int x, int y){
+    if(matriz[x][y] == '-'){
+        matriz[x][y] = '0';
+        return 0;
+    } else {
+        matriz[x][y] = 'X';
+        return 1;
     }
-
-    int escolha;
-    bool ficar = true;
-    while(ficar){
-    
-    //Mostrar as matrizes dos dois jogadores:
-    for(i = 0; i < 10; i++){
-        for(j = 0; j < 10; j++){
-            printf("%c ",P1[i][j]);
-        }
-        printf("        ");
-        for(j = 0; j < 10; j++){
-            printf("%c ", P2[i][j]);
-        }
-        printf("\n");
-    }
-
-    printf("\n   Menu do Jogo:\n\n1 - Alocar Navios\n2 - Iniciar o Jogo\n3 - Sair do Jogo\n");
-    printf("Selecione uma opcao: ");
-    scanf("%d", &escolha);
-
-    int player;
-    //Alternar entre escolhas do Menu
-    switch(escolha){
-        case 1:
-            printf("Alocar navio de qual jogador? 1 ou 2: ");
-            scanf("%d", &player);
-            if(player == 2) {
-                alocar(P2);
-            }
-            if(player == 1) {
-                alocar(P1);
-            }
-            break;
-        case 2:
-            //Inicar();
-            break;
-        case 3:
-            ficar = false;
-            break;
-        default:
-            printf("Opcao invalida");
-            break;
-    }}
-
     return 0;
 }
